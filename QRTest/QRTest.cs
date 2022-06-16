eabc27fcc8cfb8764ab4e2cbe8b8317664cde3b4
k.valder <valder.k@bk.ru>
2022-06-16 17:06:11 +0300

    edit

--- QRTest/QRTest.cs
+++ QRTest/QRTest.cs
@@ -4,17 +4,8 @@ using QRTest.Utils;
 
 namespace QRTest
 {
-    public class Tests
+    public class Tests : BaseTest
     {
-        [SetUp]
-        public void Setup()
-        {
-            var browser = AqualityServices.Browser;
-
-            browser.Maximize();
-            browser.GoTo(GetTestData.Get("BrowserData", "Url"));
-        }
-
         [Test]
         public void QRTest()
         {
@@ -23,12 +14,6 @@ namespace QRTest
             qrCoderPageObject.SetUrl(GetTestData.Get("TestValues", "UrlA1QA"));
             qrCoderPageObject.ClickSubmitBtn();
             Assert.AreEqual(GetTestData.Get("TestValues", "UrlA1QA"), FileUtils.GetValueQRCode(qrCoderPageObject.GetQRCodeSRC()), "URL different");
-        }
-
-        [TearDown]
-        public void TearDown()
-        {
-            AqualityServices.Browser.Driver.Quit();
         }
     }
 }
